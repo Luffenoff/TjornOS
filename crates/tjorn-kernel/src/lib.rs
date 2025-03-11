205e20918c9c2cd75d3be4bd8a392f5a1db9b86e
Roman <z.roma1337@gmail.com>
2025-03-11 23:05:57 +0500

    i destroy my project and i fix it

--- crates/tjorn-kernel/src/lib.rs
+++ crates/tjorn-kernel/src/lib.rs
@@ -1,10 +1,17 @@
+#![no_std]
+#![no_main]
+
+pub mod device;
+pub mod fs;
 pub mod memory;
 pub mod process;
 pub mod scheduler;
-pub mod device;
-pub mod fs;
 pub mod syscall;
 
 pub use memory::MemoryManager;
 pub use process::ProcessManager;
-pub use scheduler::Scheduler; 
+pub use scheduler::Scheduler;
+
+pub fn init() {
+    // Инициализация ядра
+}
